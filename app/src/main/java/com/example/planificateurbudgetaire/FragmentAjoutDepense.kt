4ef9092c38f80f3f0a52ba37e6d88ad076c2eb35
SimonFSmith <simfsmith@icloud.com>
2021-02-22 11:14:34 -0500

    Les labels et les inputs sont ajoutés dans le fragment d'ajout d'un revenu.

--- app/src/main/java/com/example/planificateurbudgetaire/FragmentAjoutDepense.kt
+++ app/src/main/java/com/example/planificateurbudgetaire/FragmentAjoutDepense.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_ajout_depense.view.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -34,7 +36,12 @@ class FragmentAjoutDepense : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ajout_depense, container, false)
+        val view = inflater.inflate(R.layout.fragment_ajout_depense, container, false)
+
+        view.bouton_annuler.setOnClickListener { Navigation.findNavController(view).navigate(R.id.navigateToFragmentPrincipal) }
+        view.bouton_ajouter.setOnClickListener { Navigation.findNavController(view).navigate(R.id.navigateToFragmentPrincipal) }
+
+        return view
     }
 
     companion object {
