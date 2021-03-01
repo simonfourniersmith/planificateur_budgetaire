e1a38878065cd07f5766c61855b6690f70669e11
SimonFSmith <simfsmith@icloud.com>
2021-03-01 11:29:59 -0500

    Ajout d'un fond d'écran, d'une icône, du nom de l'application ainsi qu'un document Roadmap pour lister les tâches à compléter pour arriver à l'application finale.

--- app/src/main/java/com/example/planificateurbudgetaire/MainActivity.kt
+++ app/src/main/java/com/example/planificateurbudgetaire/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.planificateurbudgetaire
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar()?.hide();
         setContentView(R.layout.activity_main)
     }
 }
